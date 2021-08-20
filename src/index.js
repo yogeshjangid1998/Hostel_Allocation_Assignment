a5aea4309233b9d4477588e7d5a09102a9849aff
rohit <rohitchittora28@gmail.com>
2021-08-20 18:08:33 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebase from 'firebase'
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAaS2aQL3zipwdIG5Vf9jF0_UscDwKS3CI",
+  authDomain: "hostelmanagementassignment.firebaseapp.com",
+  projectId: "hostelmanagementassignment",
+  storageBucket: "hostelmanagementassignment.appspot.com",
+  messagingSenderId: "42487408656",
+  appId: "1:42487408656:web:94c9a72ec3c9e5af56c295",
+  measurementId: "G-DM1JSKYZRE"
+});
 
 ReactDOM.render(
   <React.StrictMode>
